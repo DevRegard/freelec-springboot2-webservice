0275cf4295b1bb8aee1b56239eccc52a90e6350e
UTR-ADP <77965998+UTR-ADP@users.noreply.github.com>
2021-12-31 20:27:44 +0900

    Update FirstClass.java

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.print("Hello Java" +
+        System.out.print("Hello, i am Spring boot" +
                 "\n Who am are?");
+        
+        Scanner scan = new Scanner;
+        String whoIs = scan.nextLine();
     }
 }
