aa1a8ef9abf01f59f1efe02116238cbff3aaa958
UTR_ADP <utr160626@gmail.com>
2022-01-01 20:00:21 +0900

    Update FirstClass.java

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.print("Hello Java" +
+        System.out.println("Hello, i am Spring boot" +
                 "\n Who am are?");
 
+        Scanner scan = new Scanner(System.in);
+        String whoIs = scan.nextLine();
+
+        System.out.print(whoIs+", Nice me you");
     }
 }
