62ce3d0cd8f71eba8b90360d3e5b8f8138e2d385
UTR_ADP <utr160626@gmail.com>
2022-01-12 20:30:36 +0900

    HelloResponseDto.java - 테스트 DTO 생성

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter                     //선언된 모든 필드의 get 메소드 생성
+@RequiredArgsConstructor    // final 필드 -> 생성자 생성
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
