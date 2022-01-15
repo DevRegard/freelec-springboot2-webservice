908a3dfef8f7187302563da3ba3abb80fdda2721
UTR_ADP <utr160626@gmail.com>
2022-01-15 19:22:48 +0900

    HelloController - helloDto 메소드 추가 (@RequestParam)

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -20,4 +22,16 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
 }
+
+/*
+@RequestParam
+  - '외부 ~> API' 로 넘긴 파라미터 가져오기
+  - 외부에서 name(@RequestParam("name") 이란 이름으로 넘긴 파라미터를 메소드 파라미터 name(String name)에 저장
+ */
