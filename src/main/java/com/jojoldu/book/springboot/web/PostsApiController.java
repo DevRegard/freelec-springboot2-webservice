bb3e6acc966e9fadcf20602106011700472b151b
UTR_ADP <utr160626@gmail.com>
2022-01-20 20:31:54 +0900

    PostsApiController : 게시판 수정 및 아이디 찾기

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -2,10 +2,14 @@ package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping; // posts save
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping; // posts update
+import org.springframework.web.bind.annotation.GetMapping;  // posts findById
 
 @RequiredArgsConstructor
 @RestController
@@ -13,8 +17,23 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    // 게시판 등록
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
+    
+    // 게시판 수정
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+    
+    // 게시판 아이디 찾기
+    @GetMapping("/api/v1/posts/{id}")
+    public Long findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+
 }
