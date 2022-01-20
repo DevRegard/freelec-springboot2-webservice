120040037ef2602f7c6f2377a2f097d4310ac513
UTR_ADP <utr160626@gmail.com>
2022-01-20 20:33:13 +0900

    PostsResponseDto : 게시판 응답 DTO 생성

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,23 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    // 게시판 응답 DTO
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+        
+        // 'Entity' 의 필드 중 일부만 사용 -> 생성자로 Entity 받아 필드 초기화
+    }
+}
