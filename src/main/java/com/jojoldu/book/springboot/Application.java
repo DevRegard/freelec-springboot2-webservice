78d676e27899e69c45edcf01a083dc23a8a4ffe0
UTR_ADP <utr160626@gmail.com>
2022-01-21 21:50:52 +0900

    Application : JPA Auditing 활성화

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,6 +2,7 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /** 어노테이션 설명 (@SpringBootApplication, SpringApplication.run)
  @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  -내장 WAS 실행
  -톰캣 등 설치 없이, Jar 파일로 실행
  */
+@EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) { // Web Tomcat 실행
