881ee550e97466e53d4d750fe571ebc5656c1698
UTR_ADP <utr160626@gmail.com>
2022-01-21 18:39:05 +0900

    HelloController : 주석 설명 개선

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -5,18 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/*
+/** REST Controller 사용
 @RestController
  -JSON 데이터를 반환하는 컨트롤러
  -@ResponseBody 를 각 메서드 마다 선언했던 것을 생략하는 역할
  */
 @RestController
 public class HelloController {
-    /*
-    @GetMapping
+    /** 어노테이션 설명 (@GetMapping, @RequestParam)
+     @GetMapping
      - HTTP Method - Get 요청을 받을 수 있는 API
      - @RequestMapping(method = RequestMethod.GET) 역할
      - /hello 요청 -> 문자열 hello 반환
+
+     @RequestParam
+     - '외부 ~> API' 로 넘긴 파라미터 가져오기
+     - 외부에서 name(@RequestParam("name") 이란 이름으로 넘긴 파라미터를 메소드 파라미터 name(String name)에 저장
      */
     @GetMapping("/hello")
     public String hello(){
@@ -29,9 +33,3 @@ public class HelloController {
         return new HelloResponseDto(name, amount); // -> HelloController/helloDtoIsReturn()
     }
 }
-
-/*
-@RequestParam
-  - '외부 ~> API' 로 넘긴 파라미터 가져오기
-  - 외부에서 name(@RequestParam("name") 이란 이름으로 넘긴 파라미터를 메소드 파라미터 name(String name)에 저장
- */
