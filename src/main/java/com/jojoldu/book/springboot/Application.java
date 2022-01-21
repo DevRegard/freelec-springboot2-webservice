78aeefee64ae37d8f10dc88b848678e60de9d7f6
UTR_ADP <utr160626@gmail.com>
2022-01-21 21:08:12 +0900

    Application : 주석 개선(최신기술)

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -3,19 +3,18 @@ package com.jojoldu.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
-@SpringBootApplication
+/** 어노테이션 설명 (@SpringBootApplication, SpringApplication.run)
+ @SpringBootApplication
  -자동 설정 : 스프링 부트, 스프링 Bean 읽기와 생성
  -해당 어노테이션이 있는 위치부터 설정 읽음 -> 프로젝트 최상단 유지
+
+ SpringApplication.run
+ -내장 WAS 실행
+ -톰캣 등 설치 없이, Jar 파일로 실행
  */
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        /*
-        SpringApplication.run
-         -내장 WAS 실행
-         -톰캣 등 설치 없이, Jar 파일로 실행
-         */
+    public static void main(String[] args) { // Web Tomcat 실행
         SpringApplication.run(Application.class, args);
     }
 }
