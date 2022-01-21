4677c3e74fd4471f482169aecc1baeb8e6f8c261
UTR_ADP <utr160626@gmail.com>
2022-01-21 18:33:14 +0900

    Plus Commit

--- src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.jojoldu.book.springboot.web;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,6 +21,9 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat; //수동 삽입
 
+/**
+게시판 API 테스트
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //JPA 기능까지 테스트 + 랜덤 포트 실행
 public class PostsApiControllerTest {
@@ -36,7 +42,9 @@ public class PostsApiControllerTest {
         postsRepository.deleteAll();
     }
 
-    //게시판 등록 테스트
+    /**
+     게시판 등록 테스트
+     */
     @Test
     public void posts_Enrollment() throws Exception {
         //given
@@ -63,14 +71,44 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
-    
-    //게시판 수정 테스트
+
+    /**
+     게시판 수정 테스트
+     */
     @Test
     public void posts_Update() throws Exception{
         //given
+        Posts savePosts = postsRepository.save(
+                Posts.builder()
+                .title("제목1 테스트")
+                .content("글 내용1, 글 내용2, 글 내용3")
+                .author("홍길동")
+                .build()
+        );
+
+        Long updateId = savePosts.getId();
+        String expectedTitle = "제목2 테스트";
+        String expectedContent = "글 내용4, 글 내용5, 글 내용6";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
 
         //when
+        ResponseEntity<Long> responseEntity = restTemplate
+                .exchange(url, HttpMethod.PUT, requestEntity, Long.class);
 
         //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
 }
