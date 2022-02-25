a3b99b2310f17fbe3ed2a6f9f748f223ca14a113
UTR_ADP <utr160626@gmail.com>
2022-02-25 20:18:58 +0900

    change code

--- src/main/java/com/jojoldu/book/springboot/domain/user/UserRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/user/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public class UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByEmail(String email);
-}
+//public class UserRepository extends JpaRepository<User, Long> {
+//
+//    Optional<User> findByEmail(String email);
+//}
