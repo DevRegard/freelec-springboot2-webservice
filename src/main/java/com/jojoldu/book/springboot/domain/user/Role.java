55385ff67ecc69929f8274d7d2f1626decd8df58
UTR_ADP <utr160626@gmail.com>
2022-02-26 16:53:24 +0900

    관리자 권한 추가

--- src/main/java/com/jojoldu/book/springboot/domain/user/Role.java
+++ src/main/java/com/jojoldu/book/springboot/domain/user/Role.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum Role {
     GUEST("ROLE_GUEST", "손님"),
     USER("ROLE_USER", "일반 사용자"),
-    VIP("ROLE_VIP", "VIP");
+    VIP("ROLE_VIP", "VIP"),
+    MANAGER("ROLE_MANAGER", "관리자");
 
     private final String key;   // * ROLE_ 필수
     private final String title;
