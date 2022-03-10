fb384923b2adfd9c34c117ebdbb9ce5fbb1c24ad
UTR_ADP <utr160626@gmail.com>
2022-03-10 21:06:15 +0900

    첫 Mk 주석 사용 및 정리

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
+@EnableWebSecurity // Mk | Spring Security 설정들 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
     private final CustomOAuth2UserService customOAuth2UserService;
@@ -18,11 +18,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
         http
             .csrf().disable()
             .headers().frameOptions().disable()
+                // Mk | h2-console 화면 사용 위해 해당 옵션 disable [ csrf() ~ disable() ]
             .and()
             .authorizeRequests()
+                // Mk | URL 권한 관리 설정 옵션 시작점, authorizeRequests() 선언 후, antMatchers() 사용 가능
                 .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                .anyRequest().authenticated()
+                .anyRequest().authenticated() // Mk < anyRequest > : 설정된 값들 이외 나머지 URL
+                // Mk | 권한 관리 대상 지정하는 옵션 | URL, HTTP 메소드별 관리 가능
+                // Mk | "/" 등 지정된 URL 들을 permitAll() 옵션 -> 전체 열람 권한
+                // Mk | "api/v1/**" 주소 가진 API -> USER 권한 가진 사람만 가능 설정
             .and()
                .logout()
                     .logoutSuccessUrl("/")
