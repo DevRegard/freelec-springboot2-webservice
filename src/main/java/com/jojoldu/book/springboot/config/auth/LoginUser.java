6520d372480640384542b6f0123f2e650d095a66
UTR_ADP <utr160626@gmail.com>
2022-03-10 23:12:06 +0900

    어노테이션 LoginUser 생성

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/LoginUser.java
@@ -0,0 +1,12 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER) //mk- 어노테이션 생성될 수 있는위치 지정, 메서드의 파라미터로 선언된 객체에서만 사용 가능
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser //mk- 어노테이션 클래스 생성
+{
+}
