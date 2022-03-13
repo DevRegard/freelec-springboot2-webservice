639cd4fd46d60950c2b9247baa426938e9a31f1e
UTR_ADP <utr160626@gmail.com>
2022-03-13 20:43:16 +0900

    Added: 네이버 여부 판단, 네이버 생성자

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -27,13 +27,20 @@ public class OAuthAttributes
         this.picture = picture;
     }
 
-    //
-    public static OAuthAttributes of(String registrationId, String userNameAttributeName,
+    // MD) ~인지 판단하는 코드 (구글, 네이버)
+    public static OAuthAttributes of(String registrationId,
+                                     String userNameAttributeName,
                                      Map<String, Object> attributes)
     {
+        // mk) 네이버인지 판단하는 코드
+        if ("naver".equals(registrationId))
+        {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
+    // MD) 구글 생성자
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes)
     {
         return OAuthAttributes.builder()
@@ -45,7 +52,21 @@ public class OAuthAttributes
                 .build();
     }
 
-    //
+    // MD) 네이버 생성자
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes)
+    {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    // MD) 엔티티 정보
     public User toEntity()
     {
         return User.builder()
@@ -55,5 +76,4 @@ public class OAuthAttributes
                 .role(Role.GUEST)
                 .build();
     }
-
 }
