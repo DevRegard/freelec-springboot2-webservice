ff3d0fcb0c5b4ae79b6a95f4c3f4a584b994edf0
UTR_ADP <utr160626@gmail.com>
2022-03-18 21:42:53 +0900

    Update etc

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -15,7 +15,7 @@ public interface PostsRepository extends JpaRepository<Posts, Long>
 }
 
 /*
-JpaRepository<Entity 클래스, PK 타입>
+MK] JpaRepository <Entity 클래스, PK 타입>
   - CRUD 메소드 자동 생성
   - 반드시 'Entity 클래스' + 'Entity Repository' 함께 위치
  */
