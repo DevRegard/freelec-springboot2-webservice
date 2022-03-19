6abb852e2c11b6b60070e330c4e4fc5d1dc0c9c3
UTR_ADP <utr160626@gmail.com>
2022-03-19 22:44:35 +0900

    Update etc

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-/** MK] 어노테이션 설명
- Function] @SpringBootApplication
- -자동 설정 : 스프링 부트, 스프링 Bean 읽기와 생성
+/**
+ MK] @SpringBootApplication
+ -자동 설정: 스프링 부트, 스프링 Bean 읽기와 생성
  -해당 어노테이션이 있는 위치부터 설정 읽음 -> 프로젝트 최상단 유지
 
- Function] SpringApplication.run
+ MK] SpringApplication.run
  -내장 WAS 실행
  -톰캣 등 설치 없이, Jar 파일로 실행
  */
