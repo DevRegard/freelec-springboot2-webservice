d6fa46e7e297d711f55fb1de2812c2a2ca6022af
UTR_ADP <utr160626@gmail.com>
2022-03-19 22:44:42 +0900

    Update etc

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -9,30 +9,33 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-public class PostsApiController {
+public class PostsApiController
+{
 
     private final PostsService postsService;
 
-    // Method: 게시판 등록
+    // MD: 게시판 등록
     @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+    public Long save(@RequestBody PostsSaveRequestDto requestDto)
+    {
         return postsService.save(requestDto);
     }
 
-    // Method: 게시판 수정
+    // MD: 게시판 수정
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto)
     {
         return postsService.update(id, requestDto);
     }
 
-    // Method: 게시판 아이디 찾기
+    // MD: 게시판 아이디 찾기
     @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById(@PathVariable Long id) {
+    public PostsResponseDto findById(@PathVariable Long id)
+    {
         return postsService.findById(id); //findById
     }
 
-    // Method: 게시판 삭제
+    // MD: 게시판 삭제
     @DeleteMapping("/api/v1/posts/{id}")
     public Long delete(@PathVariable Long id)
     {
